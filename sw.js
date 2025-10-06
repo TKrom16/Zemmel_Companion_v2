a646932fd7c59578eef56b75ed2c2d669bb4527c
TKrom16 <thomaskrommenhoek@hotmail.com>
2025-10-06 17:47:39 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,34 @@
-const CACHE_NAME = "deadlands-cache-v1";
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/script.js",
-  "/powers.json",
-  "/edges.json",
-  "/hindrances.json",
-  "/traits.json",
-  "/icons/icon-192.png",
-  "/icons/icon-512.png"
+const CACHE_NAME = 'deadlands-cache-v1';
+const URLS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './powers.json',
+  './edges.json',
+  './hindrances.json',
+  './traits.json',
+  'https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css',
+  'https://raw.githubusercontent.com/TKrom16/Zemmel_Companion_DL/refs/heads/main/Logo.png'
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return (
+        response ||
+        fetch(event.request).then(fetchResponse => {
+          return caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, fetchResponse.clone());
+            return fetchResponse;
+          });
+        })
+      );
+    })
+  );
 });
